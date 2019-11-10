173a2eb9091ae1a0fe38c62634548c5dc7190d0c
michal <thepowerofra@gmail.com>
2019-11-10 19:51:03 +0100

    f

--- config/initializers/graphdoc.rb
+++ config/initializers/graphdoc.rb
@@ -1,6 +1,6 @@
 GraphdocRuby.configure do |config|
   config.endpoint = Rails.root.join('tmp', 'graphql', 'schema.json')
   config.output_directory = Rails.root.join('tmp', 'graphdoc').to_s
-  config.schema_name = 'MyApplicationSchema'
+  config.schema_name = 'KetoBackendSchema'
   config.run_time_generation = Rails.env.development?
 end
