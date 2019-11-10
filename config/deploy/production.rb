206ad4e8693379be1e1f175bed688a4925e1816e
michal <thepowerofra@gmail.com>
2019-11-10 20:24:11 +0100

    i

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,8 +7,6 @@ server "145.239.86.159:2202", user: "deploy", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
-
 # role-based syntax
 # ==================
 
@@ -20,8 +18,6 @@ server "145.239.86.159:2202", user: "deploy", roles: %w{app db web}
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-
-
 
 # Configuration
 # =============
