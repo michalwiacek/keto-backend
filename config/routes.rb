fa3652723b301cd7ef8e54ed9b87e07e5efb51fc
michal <thepowerofra@gmail.com>
2019-11-12 17:03:18 +0100

    posible fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  default_url_options host: ENV['HOST']
+  default_url_options host: ENV['APP_HOST']
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
