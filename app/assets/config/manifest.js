21c7e6bc5f8208128ae7d19557fb9bbdb1e507b0
michal <thepowerofra@gmail.com>
2019-11-12 15:04:44 +0100

    bug fixin

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,11 @@
+
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+//= link administrate/application.js
+//= link administrate/application.css
+//= link administrate-field-taggable/application.js
+//= link administrate-field-taggable/application.css
+//= link administrate-field-nested_has_many/application.js
+//= link administrate-field-nested_has_many/application.css
+//= link administrate-field-simple_markdown/application.js
+//= link administrate-field-simple_markdown/application.css
