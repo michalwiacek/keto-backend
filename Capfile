8d340ab033b342b43d65a3e6b218f5a2c00463b6
michal <thepowerofra@gmail.com>
2019-11-17 18:23:41 +0100

    cos przeszlo

--- Capfile
+++ Capfile
@@ -3,14 +3,14 @@ require "capistrano/setup"
 
 # Include default deployment tasks
 require "capistrano/deploy"
-# require "capistrano/rails"
+require "capistrano/rails"
 require "capistrano/console"
-# require 'capistrano/rbenv'
+require 'capistrano/rbenv'
 # require "capose"
-# require "capistrano/bundler"
-
-# set :rbenv_type, :user
-# set :rbenv_ruby, '2.6.5'
+require "capistrano/bundler"
+ 
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
 
 require "capistrano/scm/git"
 install_plugin Capistrano::SCM::Git
--- config/deploy.rb
+++ config/deploy.rb
@@ -9,14 +9,14 @@ set :repo_url, 'git@github.com:michalwiacek/keto-backend.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/#{fetch :application}"
+set :deploy_to, "/home/deploy/apps/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 append :linked_files, '.env'
 
 set :keep_releases, 5
-set :current_dir, '/currnet'
-set :current_path, "/home/deploy/#{fetch :application}"
+# set :current_dir, '/current'
+# set :current_path, "/home/deploy/#{fetch :application}"
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
@@ -44,61 +44,61 @@ set :current_path, "/home/deploy/#{fetch :application}"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-namespace :deploy do
-  desc "Initialize application"
-  task :initialize do
-    invoke 'env_file:copy'
-    invoke 'composing:build'
-    invoke 'composing:database:up'
-    invoke 'composing:database:create'
-    invoke 'composing:database:migrate'
-  end
-
-  after :published, :restart do
-    invoke 'composing:restart:web'
-    invoke 'composing:database:migrate'
-  end
-
-  before :finished, :clear_containers do
-    on roles(:app) do
-      execute "docker ps -a -q -f status=exited | xargs -r docker rm -v"
-      execute "docker images -f dangling=true -q | xargs -r docker rmi -f"
-    end
-  end
-end
-
-
-# namespace :rails do
-#   desc 'Open the rails console on primary app server'
-#   task :console do
-#     on roles(:app), primary: true do
-#       rails_env = fetch(:production)
-#       execute_interactively "#{bundle_cmd} #{current_path}/script/rails console #{rails_env}"
-#     end
+# namespace :deploy do
+#   desc "Initialize application"
+#   task :initialize do
+#     invoke 'env_file:copy'
+#     invoke 'composing:build'
+#     invoke 'composing:database:up'
+#     invoke 'composing:database:create'
+#     invoke 'composing:database:migrate'
 #   end
 
-#   desc 'Open the rails dbconsole on primary db server'
-#   task :dbconsole do
-#     on roles(:db), primary: true do
-#       rails_env = fetch(:production)
-#       execute_interactively "#{bundle_cmd} #{current_path}/script/rails dbconsole #{rails_env}"
-#     end
+#   after :published, :restart do
+#     invoke 'composing:restart:web'
+#     invoke 'composing:database:migrate'
 #   end
 
-#   def execute_interactively(command)
-#     user = fetch(:user)
-#     port = fetch(:port) || 22
-#     cmd = "ssh -l #{user} #{host} -p #{port} -t 'cd #{deploy_to}/current && #{command}'"
-#     info "Connecting to #{host}"
-#     exec cmd
-#   end
-
-#   def bundle_cmd
-#     if fetch(:rbenv_ruby)
-#       # FIXME: Is there a better way to do this? How does "execute :bundle" work?
-#       "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{File.join(fetch(:rbenv_path), '/bin/rbenv')} exec bundle exec"
-#     else
-#       'ruby '
+#   before :finished, :clear_containers do
+#     on roles(:app) do
+#       execute "docker ps -a -q -f status=exited | xargs -r docker rm -v"
+#       execute "docker images -f dangling=true -q | xargs -r docker rmi -f"
 #     end
 #   end
 # end
+
+
+namespace :rails do
+  desc 'Open the rails console on primary app server'
+  task :console do
+    on roles(:app), primary: true do
+      rails_env = fetch(:production)
+      execute_interactively "#{bundle_cmd} #{current_path}/script/rails console #{rails_env}"
+    end
+  end
+
+  desc 'Open the rails dbconsole on primary db server'
+  task :dbconsole do
+    on roles(:db), primary: true do
+      rails_env = fetch(:production)
+      execute_interactively "#{bundle_cmd} #{current_path}/script/rails dbconsole #{rails_env}"
+    end
+  end
+
+  def execute_interactively(command)
+    user = fetch(:user)
+    port = fetch(:port) || 22
+    cmd = "ssh -l #{user} #{host} -p #{port} -t 'cd #{deploy_to}/current && #{command}'"
+    info "Connecting to #{host}"
+    exec cmd
+  end
+
+  def bundle_cmd
+    if fetch(:rbenv_ruby)
+      # FIXME: Is there a better way to do this? How does "execute :bundle" work?
+      "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{File.join(fetch(:rbenv_path), '/bin/rbenv')} exec bundle exec"
+    else
+      'ruby '
+    end
+  end
+end
